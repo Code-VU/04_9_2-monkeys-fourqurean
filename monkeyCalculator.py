f88cee71f444735bc75c3dc4ef92ac3199cea7a8
Christopher Fourqurean <Chris.Fourqurean@vu.com>
2025-01-28 11:57:43 -0600

    first try

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,6 +4,14 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    # Qualifiers
+    if monkey_one.lower() == "y" and monkey_two.lower() == "y":
+        print("Uh oh! We're in trouble!")
+    elif monkey_one.lower() == "n" and monkey_two.lower() == "n":
+        print("Uh oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
+
     # end assignment
 
 
